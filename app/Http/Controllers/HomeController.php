1c77b794d124c757795785a898151f17de4f5326
nirav-2003 <145345941+nirav-2003@users.noreply.github.com>
2023-11-01 16:37:52 +0530

    api create

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Post;
+use Illuminate\Http\Request;
+
+class HomeController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    // public function __construct()
+    // {
+    //     $this->middleware('auth');
+    // }
+
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Contracts\Support\Renderable
+     */
+    public function index()
+    {
+        return view('home');
+    }
+
+    // public function saveData(Request $request)
+    // {
+    //     $request->validate([
+    //         'name' => 'required'
+    //     ]);
+
+    //     $post = new Post();
+    //     $post->name = $request->name;
+    //     $post->save();
+
+    //     return response()->json([$post, 'message' => 'save post']);
+    // }
+
+    public function getuser()
+    {
+        $post = Post::all();
+        $data = [
+            'status' => 200,
+            'post' => $post,
+            'message' => 'All Post'
+        ];
+        return response()->json([$data, 200]);
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Http\Controllers\ApiController;
+use App\Http\Controllers\HomeController;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +19,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes();
+
+// Route::get('/home', [HomeController::class, 'index']);
+// Route::post('/home', [HomeController::class, 'saveData']);
